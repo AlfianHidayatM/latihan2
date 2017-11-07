395052abee44fe359c8a83c5d6cdb67b6823dd82
AlfianHidayatM <32305314+AlfianHidayatM@users.noreply.github.com>
2017-11-07 21:04:41 +0700

    Membuat Prosuder Untuk Pengguna

--- latihan2.cpp
+++ latihan2.cpp
@@ -4,4 +4,16 @@ using namespace std;
 int main(){
 	int alas,luas,lebar,tinggi;
 	char inisial;
+	menu:
+		system("cls");
+		cout<<"==================================\n";
+		cout<<"|--Menghitung Luas Bangun Datar--|\n";
+		cout<<"==================================\n"<<endl;
+		cout<<"|----Menghitung Luas Segitiga----|\n";
+		cout<<"|-----Menghitung Luas Persegi----|\n"<<endl;
+		cout<<"Anda Bisa Memilih S/s untuk segitiga\n";
+		cout<<"Anda Bisa Memilih P/p untuk persegi\n"<<endl;
+		cout <<"Anda Mau Hitung Apa? \n"
+			 <<"'p'untuk persegi | 'S' untuk segitiga= ";
+		cin >> inisial;
 }
